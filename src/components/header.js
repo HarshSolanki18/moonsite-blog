8a7a8c6e502bb4f39da7b050a101a427b406087f
Harsh Solanki <hsolanki1884@gmail.com>
2020-07-03 16:20:26 +0530

    fixed header and now using sass

--- src/components/header.js
+++ src/components/header.js
@@ -1,51 +1,34 @@
 import React from 'react';
-import styled from '@emotion/styled';
-import { css } from '@emotion/core';
 import { Link } from 'gatsby';
+import styles from "./header.module.scss"
 
-const NavLink = styled(Link)`
-  color: #222;
-  font-size: 1rem;
-  font-weight: ${props => props.fontWeight || 'normal'};
-  line-height: 1;
-  margin: 0 0.5rem 0 0;
-  padding: 0.25rem;
-  text-decoration: none;
-  &.current-page {
-    border-bottom: 2px solid #222;
-  }
-  &:last-of-type {
-    margin-right: 0;
-  }
-`;
+
 
 const Header = () => (
-  <header
-    css={css`
-      background: #eee;
-      border-bottom: 1px solid #ddd;
-      display: flex;
-      justify-content: space-between;
-      padding: 0.5rem calc((100vw - 550px - 0.5rem) / 2);
-    `}
-  >
-    <NavLink to="/" fontWeight="bold">
-      FEM Workshop
-    </NavLink>
-    <nav
-      css={css`
-        margin-top: 0;
-      `}
-    >
-      <NavLink to="/" activeClassName="current-page">
+  <header>
+    <h1>
+    <Link to="/" activeClassName={styles.activeNav}>
+      Moonsite Blogs
+    </Link>
+    </h1>
+    <nav>
+      <ul className={styles.mainNav}>
+      <li>
+      <Link to="/" activeClassName={styles.activeNav}>
         Home
-      </NavLink>
-      <NavLink to="/about/" activeClassName="current-page">
+      </Link>  
+      </li>
+      <li>
+      <Link to="/about/" activeClassName={styles.activeNav}>
         About
-      </NavLink>
-      <NavLink to="/contact/" activeClassName="current-page">
+      </Link>
+      </li>  
+      <li>
+      <Link to="/contact/" activeClassName={styles.activeNav}>
         Contact
-      </NavLink>
+      </Link>  
+      </li>      
+      </ul>
     </nav>
   </header>
 );
